226823b3966cfcc2fc7cbbccfe78da90c9efbba8
Duncan Kelly <leucochroic@gmail.com>
2015-09-22 15:40:17 -0500

    works albeit in effeciently; spoofing retina is harder than expected

--- makeembigger.js
+++ makeembigger.js
@@ -1,8 +1,29 @@
+// (function() {
+	// alert('yo2');
+	// window = $(window).height(2880);
+	// window.width = 5120;
+	// window.devicePixelRatio = 2;
+	// chrome.windows.update(-2, {
+	// 	width: 2880,
+	// 	height: 1800
+	// }, function () {console.log("yo");});
+// })();
+	
 (function() {
-	var ogWindow = window;
-	var spoofWindow = angular.extend({}, window);
-	spoofWindow.height = 2880;
-	spoofWindow.width = 5120;
-	spoofWindow.devicePixelRatio = 2;
-	window = spoofWindow;
-})();
+	console.log('yo');
+	var app = angular.module('makeembigger', []).run(function ($interval) {
+		console.log("running");
+		$interval(function () {
+			console.log("checking");
+			var emoticons = $('.remoticon');
+			for(var i = 0; i < emoticons.length; i++)
+			{
+				var emoticon = $(emoticons[i]);
+				var src = emoticon.attr('src');
+				if(src.indexOf('@4x') == -1)
+					emoticon.attr('src', src.split('.png').join('@4x.png'));
+			}
+		}, 1000);
+	});
+	angular.element(document).ready(function() { angular.bootstrap(document, ["makeembigger"]); });
+}());
