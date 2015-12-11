2b988f83891a327085130dc3c8e157cf848b8a81
Duncan Kelly <leucochroic@gmail.com>
2015-12-11 08:30:36 -0600

    - regular images now use non intrusive resizing

--- makeembiggerredux.js
+++ makeembiggerredux.js
@@ -19,6 +19,7 @@
 	              setupImageResizing();
 	              $(document).mousedown(function (event) {
 									setupImageResizing();
+	              	$('body').append($("<div class='makeembigger-shield' style='top:0px;left:0px;width:100%;height:100%;position:fixed;display:none;background-color:transparent;'></div>"));
 								});
 	           }
 	           else if(xmlhttp.status == 400) {
@@ -42,22 +43,29 @@
 		var cachedSelect = document.onselectstart;
 
 		$(document).mouseup(function (event) {
-			makeembigger_preview_flag = false;
-			makeembigger_position = 0;
-			document.onselectstart = function (e) {return true;};
+			if(makeembigger_preview_flag) {
+				$('.makeembigger-shield').css('display', 'none');
+				event.preventDefault();
+				event.stopPropagation();
+				event.toElement = $();
+				makeembigger_preview_flag = false;
+				makeembigger_position = 0;
+				document.onselectstart = function (e) {return true;};
+				return false;
+			}
 		});
 
 		$(document).mousemove(function (event) {
 			if(makeembigger_preview_flag) {
 				var position = event.pageX;
-				var newWidth = activeResizer.position().left + (position - makeembigger_position);
-				activeResizer.css("left", newWidth+"px");
-				activeResizer.killertree.css("max-width", newWidth+"px");
-				activeResizer.killertree.css("max-height", newWidth+"px");
-				activeResizer.killertree.css("width", newWidth+"px");
-				activeResizer.killertree2.css("max-width", newWidth+"px");
-				activeResizer.killertree2.css("max-height", newWidth+"px");
-				activeResizer.killertree2.css("width", newWidth+"px");
+				var newWidth = (previewElement.killertree.width()) + (position - makeembigger_position);
+				console.log(newWidth+" = width: "+(previewElement.width())+" + (position: "+position+" - makeembigger_position: "+makeembigger_position+")");
+				previewElement.killertree.css("max-width", newWidth+"px");
+				previewElement.killertree.css("max-height", newWidth+"px");
+				previewElement.killertree.css("width", newWidth+"px");
+				// previewElement.killertree2.css("max-width", newWidth+"px");
+				// previewElement.killertree2.css("max-height", newWidth+"px");
+				// previewElement.killertree2.css("width", newWidth+"px");
 				makeembigger_position += (position - makeembigger_position);
 			}
 		});
@@ -66,21 +74,31 @@
 			element = $(element);
 			if(element.find('.makeembigger-resizer').length == 0)
 			{
-					var resizer = $("<div style='position:absolute;left:"+(element.width()+5)+"px;background-color:red;height:20px;width:20px;border-radius:2px;margin-right:10px;'></div>");
-					resizer.killertree = element.find("img");
-					resizer.killertree2 = element.find("video");
+					// var resizer = $("<div style='position:absolute;left:"+(element.width()+5)+"px;background-color:red;height:20px;width:20px;border-radius:2px;margin-right:10px;'></div>");
+					if(element.find("img").length > 0)
+						element.killertree = element.find("img");
+					else if (element.find("video").length > 0)
+						element.killertree = element.find("video");
+					else {
+						console.log("Couldn't find anything for this guy:");
+						console.log(element);
+					}
+					//console.log(element.find("img"));
+					console.log(element.killertree);
 
-					resizer.mousedown(function (event) {
+					element.mousedown(function (event) {
+						event.preventDefault();
 						makeembigger_preview_flag = true;
 						makeembigger_position = event.pageX;
-						activeResizer = resizer;
 						previewElement = element;
 						document.onselectstart = function (e) {return false;}
+						$('.makeembigger-shield').css('display', 'block');
+						return false;
 					});
 					
-					var spacer = $("<div class='makeembigger-resizer' style='height:20px;position:relative;'></div>");
-					spacer.append(resizer);
-					element.prepend(spacer);
+					// var spacer = $("<div class='makeembigger-resizer' style='height:20px;position:relative;'></div>");
+					// spacer.append(resizer);
+					// element.prepend(spacer);
 			}
 		});
 		$('.file-preview-wrapper').each(function (index, element) {
@@ -97,6 +115,7 @@
 						activeResizer = resizer;
 						previewElement = element;
 						document.onselectstart = function (e) {return false;}
+						
 					});
 					
 					var spacer = $("<div class='makeembigger-resizer' style='height:20px;position:relative;'></div>");
